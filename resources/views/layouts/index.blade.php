b95fd398619a55f5cf5c78c0eb203dd9b73db9eb
DjibrilDia1 <Djibson494@gmail.com>
2025-08-06 14:19:42 +0000

    first_commit

---
+++ resources/views/layouts/index.blade.php
@@ -0,0 +1,13 @@
+<body>
+    <header>
+        <h1>Acceuil</h1>
+    </header>
+
+    <main>
+        @yield('content')
+    </main>
+
+    <footer>
+        <p>&copy; 2025 - Mon Blog</p>
+    </footer>
+</body>
